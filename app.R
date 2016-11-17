b76d30f5ad9af315bfa7a2d478d8319f63222a13
CassidyShaffer <c.r.shaffer1@eagle.clarion.edu>
2016-11-16 19:23:43 -0500

    3rd commit

--- app.R
+++ app.R
@@ -48,9 +48,53 @@ sidebarLayout(
       
       conditionalPanel(
         condition = "input.type == 'Bull Call'",
+        selectInput("Types","Types:", c("BUY","SELL")),
         selectInput("Types","Types:", c("CALL","PUT","STOCK")),
         numericInput("dollars", "Skrike($)", 0, min = 0, max = 3000),
         numericInput("dollars", "Price($)", 0, min = 0, max = 3000),
+        selectInput("Types","Types:", c("BUY","SELL")),
+        selectInput("Types","Types:", c("CALL","PUT","STOCK")),
+        numericInput("dollars", "Skrike($)", 0, min = 0, max = 3000),
+        numericInput("dollars", "Price($)", 0, min = 0, max = 3000)
+      ),
+      
+      conditionalPanel(
+        condition = "input.type =='Bear Call'",
+        selectInput("Types","Types:", c("BUY","SELL")),
+        selectInput("Types","Types:", c("CALL","PUT","STOCK")),
+        numericInput("dollars", "Skrike($)", 0, min = 0, max = 3000),
+        numericInput("dollars", "Price($)", 0, min = 0, max = 3000),
+        selectInput("Types","Types:", c("BUY","SELL")),
+        selectInput("Types","Types:", c("CALL","PUT","STOCK")),
+        numericInput("dollars", "Skrike($)", 0, min = 0, max = 3000),
+        numericInput("dollars", "Price($)", 0, min = 0, max = 3000)
+      ),
+      
+      conditionalPanel(
+        condition = "input.type == 'Straddle'",
+        selectInput("Types","Types:", c("BUY","SELL")),
+        selectInput("Types","Types:", c("CALL","PUT","STOCK")),
+        numericInput("dollars", "Skrike($)", 0, min = 0, max = 3000),
+        numericInput("dollars", "Price($)", 0, min = 0, max = 3000),
+        selectInput("Types","Types:", c("BUY","SELL")),
+        selectInput("Types","Types:", c("CALL","PUT","STOCK")),
+        numericInput("dollars", "Skrike($)", 0, min = 0, max = 3000),
+        numericInput("dollars", "Price($)", 0, min = 0, max = 3000)
+      ),
+      
+      conditionalPanel(
+        condition = "input.type == 'Butterfly'",
+        selectInput("Types","Types:", c("BUY","SELL")),
+        # since a butterfly spread is buy 1 call sell 2 call then buy 1 call. 
+        # how should this one be completed. 
+        selectInput("Types","Types:", c("CALL","PUT","STOCK")),
+        numericInput("dollars", "Skrike($)", 0, min = 0, max = 3000),
+        numericInput("dollars", "Price($)", 0, min = 0, max = 3000),
+        selectInput("Types","Types:", c("BUY","SELL")),
+        selectInput("Types","Types:", c("CALL","PUT","STOCK")),
+        numericInput("dollars", "Skrike($)", 0, min = 0, max = 3000),
+        numericInput("dollars", "Price($)", 0, min = 0, max = 3000),
+        selectInput("Types","Types:", c("BUY","SELL")),
         selectInput("Types","Types:", c("CALL","PUT","STOCK")),
         numericInput("dollars", "Skrike($)", 0, min = 0, max = 3000),
         numericInput("dollars", "Price($)", 0, min = 0, max = 3000)
@@ -60,14 +104,14 @@ sidebarLayout(
       
     )))
 
-   mainPanel(
-     renderPlot(
-       if(input$type == "Protective Put")
-     )
-   )
+   
 
 server <- function(input, output) 
   
-
+  mainPanel(
+    renderPlot(
+      if(input$type == "Protective Put")
+    )
+  )
     
 shinyApp(ui = ui, server = server)
